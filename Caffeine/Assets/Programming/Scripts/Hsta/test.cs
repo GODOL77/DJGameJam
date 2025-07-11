801e47345089ccb295a45d30392440d66093300e
Hsta2614 <dh961231@gmail.com>
2025-07-11 20:54:56 +0900

    주석테스트

---
+++ Caffeine/Assets/Programming/Scripts/Hsta/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    //한글주석
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
